d673019ccb7d57ad6cc195ac6fea7c00e3473108
Arbnor Memedi <arbnormemedi@outlook.de>
2022-09-01 11:50:52 +0200

    Demo Page hinzugefügt.

--- datingAppByAJA/testPageDB.xaml.cs
+++ datingAppByAJA/testPageDB.xaml.cs
@@ -9,17 +9,69 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using MySqlConnector;
 
 namespace datingAppByAJA
 {
     /// <summary>
     /// Interaktionslogik für testPageDB.xaml
     /// </summary>
+    /// Made by Anton, Joshua and Arbnor
     public partial class testPageDB : Window
     {
+        string serverMySql = "datingapp.mysql.arbnor.me";
+        string userIdMySql = "root";
+        string passwordMySql = "frVnoGZ53KaBZ58L9428";
+        string databaseMySql = "datingApp";
         public testPageDB()
         {
             InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string password = passwortEingabe.Text;
+            string email = emailEingabe.Text;
+            var con =
+                new MySqlConnection($"server={serverMySql};user id={userIdMySql};password={passwordMySql};database={databaseMySql}");
+
+            string query = $"Insert into userTable(password, email)" +
+                $" values('{password}','{email}')";
+            MessageBox.Show("Daten geschrieben");
+            var command = new MySqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            string email = emailEingabe.Text;
+            var con =
+                new MySqlConnection($"server={serverMySql};user id={userIdMySql};password={passwordMySql};database={databaseMySql}");
+
+            string query = $"SELECT * FROM datingApp.userTable WHERE email LIKE {email}";
+            MessageBox.Show("Daten gelesen");
+            var command = new MySqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
         }
     }
 }

dc9fd3a62b66e432b0efc09e6a156398967c0c3e
KaenguruKnecht <Joshua.Jankowski@gmx.de>
2022-09-01 12:20:42 +0200

    Registrierungfenster

---
+++ datingAppByAJA/Registrierung.xaml.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using MySqlConnector;
+
+namespace datingAppByAJA
+{
+    /// <summary>
+    /// Interaktionslogik für Registrierung.xaml
+    /// </summary>
+    public partial class Registrierung : Page
+    {
+        public Registrierung()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            string password = passwortEingabe.Text;
+            string email = emailEingabe.Text;
+            MessageBox.Show("Es wird jetzt zum Server verbunden");
+            var con =
+                new MySqlConnection("server=datingapp.mysql.arbnor.me;user id=root;password=frVnoGZ53KaBZ58L9428;database=datingApp");
+
+            string query = $"Insert into userTable(password, email)" +
+                $" values('{password}','{email}')";
+            MessageBox.Show("Daten geschrieben");
+            var command = new MySqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+                command.ExecuteNonQuery();
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}

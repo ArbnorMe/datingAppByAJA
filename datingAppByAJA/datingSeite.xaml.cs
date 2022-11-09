86429a98c1a4b4ac232c99379afc26384f33b1e6
Arbnor Memedi <arbnormemedi@outlook.de>
2022-11-09 12:11:39 +0100

    - Bild Upload (noch nicht funktional!)

--- datingAppByAJA/datingSeite.xaml.cs
+++ datingAppByAJA/datingSeite.xaml.cs
@@ -27,6 +27,7 @@ namespace datingAppByAJA
             InitializeComponent();
         }
 
+        // Image Location
         public string imgLocation = "";
 
         private void Favorisieren_Button_Click(object sender, RoutedEventArgs e)
@@ -37,34 +38,58 @@ namespace datingAppByAJA
 
         private void Btn_Upload_Click(object sender, RoutedEventArgs e)
         {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Title = "Open Picture";
+            open.Multiselect = false;
+            open.Filter = "Image Files(*.jpg; *.jpeg; *.gif; *.bmp)|*.jpg; *.jpeg; *.gif; *.bmp";
+
+            if (open.ShowDialog() == true)
+            {
+                try
+                {
+                    bi = new BitmapImage();
+                    bi.BeginInit();
+                    bi.UriSource = new Uri(open.FileName, UriKind.RelativeOrAbsolute);
+                    imgLocation = open.FileName;
+                    bi.EndInit();
+
+                    pictureBox.Source = bi;
+                }
+                catch (Exception ex)
+                {
+                    Console.Write("Exception: " + ex);
+                }
+            }
+        }
+       
+
+        private void Btn_Save_Click(object sender, RoutedEventArgs e)
+        {
             // MySQL verbindung
             var connection = new MySqlConnection($"server={DBVerbindung.serverMySql};user id={DBVerbindung.userIdMySql};password={DBVerbindung.passwordMySql};database={DBVerbindung.databaseMySql}");
-            // Image location
-            string imgLocation = "";
 
             MySqlCommand cmd;
 
             try
             {
                 byte[] images = null;
-                FileStream streem = new FileStream(imgLocation, FileMode.Open, FileAccess.Read);
+                FileStream streem = new FileStream(imgLocation, FileMode.Open, FileAccess.Read); // Hier findet der Fehler statt
                 BinaryReader brs = new BinaryReader(streem);
                 images = brs.ReadBytes((int)streem.Length);
 
                 connection.Open();
-                string sqlQuery = "Insert into bioData(email,Name,Image)Values'" + UserDaten.email + "','" + UserDaten.username + "Profilbild',@images";
+                string sqlQuery = $"Insert into {DBVerbindung.userpicturesTable}(email,Name,Image)Values'" + UserDaten.email + "','" + UserDaten.username + "Profilbild',@images";
                 cmd = new MySqlCommand(sqlQuery, connection);
-                cmd.Parameters.Add(new SqlParameter(""))
+                cmd.Parameters.Add(new MySqlParameter("@images", images));
+                int n = cmd.ExecuteNonQuery();
+                connection.Close();
+                MessageBox.Show(n.ToString() + " Datei wurde erfolgreich gesichert....... ");
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 // Fehleranzeige
                 MessageBox.Show(ex.Message);
             }
-        }
-
-        private void Btn_Save_Click(object sender, RoutedEventArgs e)
-        {
         }
     }
   }
